cf617deb3ba5d2b7e5d4a7c69edd00457ff5ae0e
abfalex <87718997+abfalex@users.noreply.github.com>
2024-07-03 13:48:43 +0300

    optimize post details and tag filter

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def get_likes_count(post):
 
 
 def get_related_posts_count(tag):
-    return tag.posts.count()
+    return tag.tags_count
 
 
 def serialize_post(post):
@@ -43,7 +43,7 @@ def index(request):
     fresh_posts = Post.objects.order_by("-published_at")
     most_fresh_posts = list(fresh_posts)[:5]
 
-    tags = Tag.objects.all()
+    tags = Tag.objects.annotate(tags_count=Count("posts"))
     popular_tags = sorted(tags, key=get_related_posts_count, reverse=True)
     most_popular_tags = popular_tags[:5]
 
@@ -84,11 +84,15 @@ def post_detail(request, slug):
         "tags": [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(tags_count=Count("posts"))
+    popular_tags = sorted(tags, key=get_related_posts_count, reverse=True)
+    most_popular_tags = popular_tags[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = sorted(
+        Post.objects.annotate(likes_count=Count("likes")),
+        key=get_likes_count,
+        reverse=True,
+    )[:5]
 
     context = {
         "post": serialized_post,
@@ -101,11 +105,15 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(tags_count=Count("posts"))
+    popular_tags = sorted(tags, key=get_related_posts_count, reverse=True)
+    most_popular_tags = popular_tags[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = sorted(
+        Post.objects.annotate(likes_count=Count("likes")),
+        key=get_likes_count,
+        reverse=True,
+    )[:5]
 
     related_posts = tag.posts.all()[:20]
 
