ca3b9ce4d066793d4230b57df00af9e57aa6fe1c
abfalex <87718997+abfalex@users.noreply.github.com>
2024-07-04 09:07:17 +0300

    add popular tags manager

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.db.models import Count
 from django.urls import reverse
 from django.contrib.auth.models import User
+
+
+class TagQuerySet(models.QuerySet):
+    def popular(self):
+        return self.annotate(tags_count=Count("posts")).order_by("-tags_count")
 
 
 class Post(models.Model):
@@ -35,6 +41,8 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField("Тег", max_length=20, unique=True)
+
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
